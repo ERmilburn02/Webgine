e3213585fe2ab867d5dd63f3154dd9157f85d581
Eliza Milburn <61363431+ERmilburn02@users.noreply.github.com>
2022-02-10 14:14:32 +0000

    Configuration

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              configFile: "web.tsconfig.json",
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "webgine.js",
+    path: path.resolve(__dirname, ".", "dist", "web"),
+  },
+};
